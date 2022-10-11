87b575832c13c6c2deadb7559ca7620c5f61c608
Elias <eliasnemr@gmail.com>
2022-10-11 07:26:09 +0300

    first commit, fundamentals implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { useAppDispatch } from "./redux/hooks";
+import { showToast } from "./redux/slices/app/toastSlice";
+import { callAndStoreCoins } from "./redux/slices/minima/coinSlice";
+
+import { callAndStoreWalletBalance } from "./redux/slices/minima/balanceSlice";
+import { events } from "./minima/libs/events";
+import FormFutureCash from "./components/forms/FormFutureCash";
+import { addFutureCashScript } from "./minima/rpc-commands";
+import { futureCashScript } from "./minima/scripts";
+import FutureCoins from "./components/pages/FutureCoins";
+import { callAndStoreChainHeight } from "./redux/slices/minima/statusSlice";
 
 function App() {
+  const dispatch = useAppDispatch();
+  React.useEffect(() => {
+    events.onInit(() => {
+      console.log("Minima inited");
+      dispatch(callAndStoreChainHeight());
+      dispatch(callAndStoreCoins());
+      dispatch(callAndStoreWalletBalance());
+      addFutureCashScript(futureCashScript, false).then(() => {
+        dispatch(showToast("FutureCash script added.", "info", ""));
+      });
+    });
+
+    events.onNewBlock(() => {
+      dispatch(callAndStoreChainHeight());
+    });
+
+    events.onNewBalance(() => {
+      dispatch(callAndStoreCoins());
+      dispatch(callAndStoreWalletBalance());
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormFutureCash />
+      <FutureCoins />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+
+import { store as appStore } from "./redux/store";
+import { HashRouter } from "react-router-dom";
+
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+import { CssBaseline } from "@mui/material";
+
+import MiSnackBar from "./components/MiSnackBar";
+
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { LocalizationProvider } from "@mui/x-date-pickers";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <ThemeProvider theme={theme}>
+        <Provider store={appStore}>
+          <HashRouter>
+            <CssBaseline />
+            <App />
+            <MiSnackBar />
+          </HashRouter>
+        </Provider>
+      </ThemeProvider>
+    </LocalizationProvider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
