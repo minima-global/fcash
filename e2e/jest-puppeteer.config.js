fef1391a20598cd4c96873b20105add8ecde7c81
dynamitesushi <winson.hero@gmail.com>
2022-11-21 10:30:10 +0000

    fixed headless in jest puppeteer config file

--- e2e/jest-puppeteer.config.js
+++ e2e/jest-puppeteer.config.js
@@ -3,11 +3,11 @@ require('dotenv').config({ path: '../.env' });
 module.exports = {
     launch: {
         devtools: false,
-        headless: process.env.HEADLESS === '1',
+        headless: !process.env.HEADLESS ? false : process.env.HEADLESS === '1',
         ignoreHTTPSErrors: true,
         defaultViewport: {
-            width:1920,
-            height:1080
+            width: 1920,
+            height: 1080
         },
         args: [
             '--ignore-certificate-errors',
